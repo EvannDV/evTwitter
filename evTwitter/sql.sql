0e08d26d6eef4f439344486f954557ee664dd4c1
Evann Dv <111243677+EvannDV@users.noreply.github.com>
2022-12-16 22:02:14 +0100

    Add files via upload

---
+++ evTwitter/sql.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 16 déc. 2022 à 21:41
+-- Version du serveur : 10.4.25-MariaDB
+-- Version de PHP : 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `evdev`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `evtwitter`
+--
+
+CREATE TABLE `evtwitter` (
+  `name` varchar(255) NOT NULL,
+  `mdp` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `evtwitter`
+--
+
+
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `evtwitter`
+--
+ALTER TABLE `evtwitter`
+  ADD PRIMARY KEY (`name`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
